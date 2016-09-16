e5f46e08d58df69586a10213190d34b52c28de4a
Natalia Hess <nathess91@gmail.com>
2016-09-16 16:00:59 -0700

    adds files via upload

---
+++ project-1/app.js
@@ -0,0 +1,12 @@
+// var game = {
+//   board: {
+//     rows: 7,
+//     columns: 6,
+//     players: 2
+//   },
+//   player: {
+//     name: "value",
+//     pieces: 42,
+//     placePiece: function()
+//   }
+// }

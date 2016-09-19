dcd9a092e86268aa97add93f9c53fb07230bb256
Natalia Hess <nathess91@gmail.com>
2016-09-18 22:15:20 -0700

    refactors code

--- project-1/app.js
+++ project-1/app.js
@@ -1,41 +1,44 @@
 $(document).ready(function(){
-  var turnCount = 0;
+  var turnCounter = 0;
 
   $("#board").find("td").on("click", function(){
     // Player 1's turn
-    if (turnCount % 2 === 0){
+    if (turnCounter % 2 === 0){
       $(this).text("X");
       checkWinner("X");
     } else {
       //player 2's turn
       $(this).text("O");
-        checkWinner("O");
+      checkWinner("O");
     }
 
-    turnCount++;
+    turnCounter++;
   });
 
+  function winAndReset(player) {
+    alert("Game over! "+player+" wins!");
+    $("#newGame").removeClass("invisible");
+  }
+
   function checkWinner(player){
+
     // Check if top row match
      if ($("#board").find("#1").text() !== ""){
        if ($("#board").find("#1").text() == $("#board").find("#2").text()) {
          if ($("#board").find("#1").text() == $("#3").text()) {
-           alert("Game over! "+player+" is the winner!");
-           $("#newGame").removeClass("invisible");
+           winAndReset(player);
          }
       }
     // Check if left column match
      if ($("#board").find("#1").text() == $("#board").find("#4").text()) {
       if ($("#board").find("#1").text() == $("#7").text()) {
-        alert("Game over! "+player+" is the winner!");
-        $("#newGame").removeClass("invisible");
+        winAndReset(player);
       }
     }
     // Check if left diagonal match
     if ($("#board").find("#1").text() == $("#board").find("#5").text()) {
       if ($("#board").find("#1").text() == $("#9").text()) {
-        alert("Game over! "+player+" is the winner!");
-        $("#newGame").removeClass("invisible");
+        winAndReset(player);
       }
     }
   }
@@ -43,8 +46,7 @@ $(document).ready(function(){
    if ($("#board").find("#2").text() !== ""){
      if ($("#board").find("#2").text() == $("#board").find("#5").text()) {
        if ($("#board").find("#2").text() == $("#8").text()) {
-         alert("Game over! "+player+" is the winner!");
-         $("#newGame").removeClass("invisible");
+         winAndReset(player);
        }
      }
    }
@@ -53,15 +55,13 @@ $(document).ready(function(){
    if ($("#board").find("#3").text() !== ""){
      if ($("#board").find("#3").text() == $("#board").find("#6").text()) {
        if ($("#board").find("#3").text() == $("#9").text()) {
-         alert("Game over! "+player+" is the winner!");
-         $("#newGame").removeClass("invisible");
+         winAndReset(player);
        }
      }
     // Check if right diagonal match
     if ($("#board").find("#3").text() == $("#board").find("#5").text()) {
       if ($("#board").find("#3").text() == $("#7").text()) {
-        alert("Game over! "+player+" is the winner!");
-        $("#newGame").removeClass("invisible");
+        winAndReset(player);
       }
     }
   }
@@ -70,8 +70,7 @@ $(document).ready(function(){
     if ($("#board").find("#4").text() !== ""){
       if ($("#board").find("#4").text() == $("#board").find("#5").text()) {
         if ($("#board").find("#4").text() == $("#6").text()) {
-           alert("Game over! "+player+" is the winner!");
-           $("#newGame").removeClass("invisible");
+           winAndReset(player);
         }
       }
     }
@@ -80,8 +79,7 @@ $(document).ready(function(){
     if ($("#board").find("#7").text() !== ""){
       if ($("#board").find("#7").text() == $("#board").find("#8").text()) {
         if ($("#board").find("#7").text() == $("#9").text()) {
-          alert("Game over! "+player+" is the winner!");
-          $("#newGame").removeClass("invisible");
+          winAndReset(player);
         }
       }
     }
